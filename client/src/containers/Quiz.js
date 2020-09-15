4559794cae17e2d22df3f7d76ad6a1ab7d51d57c
Jon Le Marquand <jon.lemarquand@gmail.com>
2020-09-15 16:20:51 +0100

    Added 5 second timer

--- client/src/containers/Quiz.js
+++ client/src/containers/Quiz.js
@@ -7,7 +7,6 @@ import ScoreButton from '../components/Button/ScoreButton';
 import '../App.scss';
 import tick from '../assets/tick.svg';
 import cross from '../assets/cross.svg';
-import { render } from 'react-dom';
 
 const Quiz = () => {
 
@@ -16,8 +15,12 @@ const Quiz = () => {
     const [questionNumber, setQuestionNumber] = useState(1);
     const [rightScore, setRightScore] = useState(0);
     const [wrongScore, setWrongScore] = useState(0);
+    const [countdown, setCountdown] = useState(6);
+    const [showAnswer, setShowAnswer] = useState(false);
 
     const callQuestionAPI = () => {
+        setShowAnswer(false);
+        setCountdown(6);
         fetch("http://localhost:8000/newquestion")
             .then(res => res.json())
             .then(res => {
@@ -31,16 +34,49 @@ const Quiz = () => {
         callQuestionAPI();
     }, [questionNumber]);
 
+    useEffect(() => {
+        let interval = null;
+        if (countdown === 1) {
+            clearInterval(interval);
+            setShowAnswer(true);
+        } else if (!showAnswer) {
+            interval = setInterval(() => {
+              setCountdown(countdown => countdown - 1);
+            }, 1000);
+        }
+        return () => clearInterval(interval);
+      }, [showAnswer, countdown]);
+
+    const Answer = () => {
+        if (showAnswer) {
+            return (
+                <div className="post answer">
+                { answer }
+                    <div className="rightWrong">
+                        <ScoreButton url={tick} alt="tick" styling="rightTick" changeScore={changeScore}/>
+                        <ScoreButton url={cross} alt="cross" styling="wrongCross" changeScore={changeScore}/>
+                    </div>
+                </div>
+
+            )
+        } else if (!showAnswer) {
+            return (
+                <div className="pre answer">
+                    <div className="waitText">Answer in...</div>
+                    <div id="timer">{countdown - 1}</div>
+                </div>
+            )
+        } else return null;
+    }
+
 
     const changeScore = (e) => {
         if (e.target.dataset.value === "rightTick") {
             setRightScore(rightScore + 1);
-            setQuestionNumber(questionNumber + 1);
-            //setRightScore( rightScore =+ 1);
         } else if (e.target.dataset.value === "wrongCross") {
             setWrongScore(wrongScore + 1);
-            setQuestionNumber(questionNumber + 1);
         }
+        setQuestionNumber(questionNumber + 1);
     }
 
     return (
@@ -49,17 +85,7 @@ const Quiz = () => {
                 <HeaderLogo />
                 <h2>{ question }</h2>
             </div>
-            <div className="pre answer hide">
-                <div className="waitText hide">Answer in...</div>
-                <div id="timer hide">&nbsp;</div>
-            </div>
-            <div className="post answer">
-                { answer }
-      </div>
-            <div className="rightWrong">
-                <ScoreButton url={tick} alt="tick" styling="rightTick" changeScore={changeScore}/>
-                <ScoreButton url={cross} alt="cross" styling="wrongCross" changeScore={changeScore}/>
-            </div>
+                <Answer />
             <Score rightScore={rightScore} wrongScore={wrongScore}/>
         </div>
     );
